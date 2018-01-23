12861277159405c17243b62f8131640a8d842f86
stevegordon <tdtsgordon@gmail.com>
2018-01-23 13:09:24 -0800

    add tv and corkboard

--- mystuff/app.js
+++ mystuff/app.js
@@ -1,3 +1,9 @@
 (function(){
   console.log("Javascript says hello");
+  var tv = document.getElementById("screen");
+  startGame = function(){
+    tv.style.backgroundImage = "url('start.gif')";
+    console.log("changed");
+  };
+  setTimeout(startGame,5000);
 })();
